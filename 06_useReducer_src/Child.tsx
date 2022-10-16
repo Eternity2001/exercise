4b8670be060208436fc916b3c5d11eaec42ca2c1
Eternity <2901174640@qq.com>
2022-10-16 10:37:10 +0800

    Hooks-useReducer

---
+++ 06_useReducer_src/Child.tsx
@@ -0,0 +1,19 @@
+import { forwardRef, useImperativeHandle } from 'react'
+
+// 函数组件传入ref会当作props处理，需要用forwardRef函数处理
+export const Child = forwardRef((props, ref) => {
+  // ref不可以直接获取到组件方法
+  // 需要使用useImperativeHandle
+  // 主动暴露
+  useImperativeHandle(ref, () => ({
+    childFn: fn
+  }))
+  const fn = (): void => {
+    console.log('子组件方法')
+  }
+  return (
+      <div>
+        <h3>Child组件</h3>
+      </div>
+  )
+})
